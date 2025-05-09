efcfa8fd1464f9d06e2ef009bd2f2f3c205954e8
oritp <orioltp@gmail.com>
2025-05-09 13:02:13 +0200

    Update vscode.sh

--- install/vscode.sh
+++ install/vscode.sh
@@ -8,6 +8,13 @@ echo "========================================="
 echo -e "\nThis script is going to install Visual Studio Code in your system."
 read -p "Are you ready to proceed? (y/n) " answer
 
+if [[ "$answer" != "y" && "$answer" != "Y" ]]; then
+    echo -e "The installation has been stopped. Bye! \n"
+    exit 0
+fi
+
+echo -e "\nStarting the Visual Studio Code installation...\n"
+
 # Install requierements
 sudo apt-get update
 sudo apt-get install -y wget gpg apt-transport-https
