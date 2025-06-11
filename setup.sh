31267a2ce305fe56b16abe84bdd661d45d873557
Oriol Torres Pla <100279838+oritp@users.noreply.github.com>
2025-06-12 00:20:57 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,11 +9,11 @@ print "#                                                          #"
 print "############################################################"
 
 # Enabling execution permissions
-chmod +x essentials.sh
-chmod +x terminal.sh
-chmod +x appearance.sh
-chmod +x vscode.sh
-chmod +x spotify.sh
+chmod +x ./install/essentials.sh
+chmod +x ./terminal/terminal.sh
+chmod +x ./install/appearance.sh
+chmod +x ./install/vscode.sh
+chmod +x ./install/spotify.sh
 
 print "\n[+] Welcome to Debian 12 Setup Menu!"
 print "[+] What do you want to install?"
@@ -29,12 +29,12 @@ print ""
 
 case $option in
     1) bash install/essentials.sh
-       bash install/terminal.sh
+       bash terminal/terminal.sh
        bash install/appearance.sh
        bash install/vscode.sh
        bash install/spotify.sh ;;
     2) bash install/essentials.sh ;;
-    3) bash install/terminal.sh ;;
+    3) bash terminal/terminal.sh ;;
     4) bash install/appearance.sh ;;
     5) bash install/vscode.sh ;;
     6) bash install/spotify.sh ;;
